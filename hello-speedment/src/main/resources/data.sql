41e881c8278d910af63db5558a404592c10ab4c2
Per-Åke Minborg <minborg@speedment.com>
2017-09-11 14:53:52 -0700

    Remove module hello-speedment

--- hello-speedment/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-insert into `user` (`id`, `mail`) values (1, 'alice@company.com');
-insert into `user` (`id`, `mail`) values (2, 'bob@company.com');
-insert into `user` (`id`, `mail`) values (3, 'charlie@company.com');
-insert into `user` (`id`, `mail`) values (4, 'daniella@company.com');
-
-insert into `image` (`id`, `title`, `description`, `author`, `published`, `src`) values (1, 'Butterfly','A nice butterfly', 1 ,now(),'http://dreamatico.com/data_images/butterfly/butterfly-3.jpg');
-insert into `image` (`id`, `title`, `description`, `author`, `published`, `src`) values (2, 'Ferarri','An Enzo Ferarri, so cool!', 1 ,now(),'http://autoworld.com/UploadedImages/photo/1372313871.jpg');
